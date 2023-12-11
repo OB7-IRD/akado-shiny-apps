ce6c66e8da39ddbd3d9bdfbf85f20471ab55db18
OceaneBouhineau <oceane.bouhineau@ird.fr>
2023-12-11 11:55:13 +0100

    Remove virtual harbour in SQL

--- inst/sql/activity.sql
+++ inst/sql/activity.sql
@@ -26,6 +26,6 @@ FROM
     LEFT JOIN ps_common.vesselactivity va ON a.vesselactivity = va.topiaid 
     LEFT JOIN common.ocean o ON t.ocean = o.topiaid 
     LEFT JOIN (SELECT za.ocean, ST_Union(za.geom) AS geom FROM custom.zfao za WHERE f_level = 'MAJOR' GROUP BY za.ocean) AS tmp_geo ON ST_Intersects(tmp_geo.geom, a.the_geom) = TRUE 
-    LEFT JOIN (SELECT ST_Buffer(h.the_geom,0.2) AS harbour_area, h.topiaid FROM common.harbour h) AS tmp_harbour ON ST_Intersects(tmp_harbour.harbour_area, a.the_geom) = TRUE
+    LEFT JOIN (SELECT ST_Buffer(h.the_geom,0.2) AS harbour_area, h.topiaid FROM common.harbour h WHERE h.locode IS NOT NULL) AS tmp_harbour ON ST_Intersects(tmp_harbour.harbour_area, a.the_geom) = TRUE
 WHERE 
     a.topiaid IN (?select_item) 
--- inst/sql/activity_harbour.sql
+++ inst/sql/activity_harbour.sql
@@ -4,6 +4,6 @@ SELECT
     tmp_harbour.topiaid::text AS harbour_id
 FROM 
     ps_logbook.activity a
-     INNER JOIN (SELECT ST_Buffer(h.the_geom,0.2) AS harbour_area, h.topiaid FROM common.harbour h) AS tmp_harbour ON ST_Intersects(tmp_harbour.harbour_area, a.the_geom) = TRUE
+     INNER JOIN (SELECT ST_Buffer(h.the_geom,0.2) AS harbour_area, h.topiaid FROM common.harbour h WHERE h.locode IS NOT NULL) AS tmp_harbour ON ST_Intersects(tmp_harbour.harbour_area, a.the_geom) = TRUE
 WHERE 
     a.topiaid IN (?select_item) 
