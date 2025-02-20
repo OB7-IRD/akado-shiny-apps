d06fa41665daaaaa88e131519e0bafbcb6e51827
OceaneBouhineau <oceane.bouhineau@ird.fr>
2025-02-20 09:57:38 +0100

    Add argument check_operation_inspector
    Add argument for check_operation_inspector function :
    - Add vessel_activity
    - Add school_type
    - Add success_status_school_type
    - Add success_status_weight
    No change result

--- R/function.R
+++ R/function.R
@@ -1432,6 +1432,10 @@ check_fishing_context_inspector <- function(dataframe1,
 #' @description The purpose of the check_operation_inspector function is to provide a table of data that contains an inconsistency with succes status and vessel activity, the type of school or the weight caught
 #' @param dataframe1 {\link[base]{data.frame}} expected. Csv or output of the function {\link[furdeb]{data_extraction}}, which must be done before using the check_operation_inspector () function.
 #' @param output {\link[base]{character}} expected. Kind of expected output. You can choose between "message", "report" or "logical".
+#' @param vessel_activity {\link[base]{character}} expected. Default values: c("6"). Vector of inventory of codes for activities that must have a success status
+#' @param school_type {\link[base]{character}} expected, default : c("0"). Vector of inventory of code of the school type must not have specific success status (defined success_status_school_type)
+#' @param success_status_school_type {\link[base]{character}} expected, default : c("0", "1"). Vector of inventory of code of the success status must not have specific school type (defined school_type)
+#' @param success_status_weight {\link[base]{character}} expected, default : c("0"). Vector of inventory of code of the success status must not have weigth
 #' @return The function returns a {\link[base]{character}} with output is "message", a {\link[base]{data.frame}} with output is "report", a {\link[base]{logical}} with output is "logical"
 #' @details
 #' The input dataframe must contain all these columns for the function to work :
@@ -1445,7 +1449,11 @@ check_fishing_context_inspector <- function(dataframe1,
 #' }
 #' @export
 check_operation_inspector <- function(dataframe1,
-                                      output) {
+                                      output,
+                                      vessel_activity = c("6"),
+                                      school_type = c("0"),
+                                      success_status_school_type = c("0", "1"),
+                                      success_status_weight = c("0")) {
   # 0 - Global variables assignement ----
   vesselactivity_code <- NULL
   successstatus_code <- NULL
@@ -1487,31 +1495,73 @@ check_operation_inspector <- function(dataframe1,
       output = "message"
     ))
   }
+  if (!codama::r_type_checking(
+    r_object = vessel_activity,
+    type = "character",
+    output = "logical"
+  )) {
+    return(codama::r_type_checking(
+      r_object = vessel_activity,
+      type = "character",
+      output = "message"
+    ))
+  }
+  if (!codama::r_type_checking(
+    r_object = school_type,
+    type = "character",
+    output = "logical"
+  )) {
+    return(codama::r_type_checking(
+      r_object = school_type,
+      type = "character",
+      output = "message"
+    ))
+  }
+  if (!codama::r_type_checking(
+    r_object = success_status_school_type,
+    type = "character",
+    output = "logical"
+  )) {
+    return(codama::r_type_checking(
+      r_object = success_status_school_type,
+      type = "character",
+      output = "message"
+    ))
+  }
+  if (!codama::r_type_checking(
+    r_object = success_status_weight,
+    type = "character",
+    output = "logical"
+  )) {
+    return(codama::r_type_checking(
+      r_object = success_status_weight,
+      type = "character",
+      output = "message"
+    ))
+  }
   select <- dataframe1$activity_id
   nrow_first <- length(unique(select))
   # 2 - Data design ----
   # Indicates whether the vessel activity requires a success status
-  dataframe1$threshold <- "6"
   comparison_successstatus_vesselactivity <- codama::vector_comparison(
     first_vector = dataframe1$vesselactivity_code,
-    second_vector = dataframe1$threshold,
-    comparison_type = "equal",
+    second_vector = vessel_activity,
+    comparison_type = "difference",
     output = "report"
   )
   dataframe1$logical_successstatus_vesselactivity <- comparison_successstatus_vesselactivity$logical
   # Case of success status NA: must not have activity 6 (inverse of the result obtained)
   dataframe1$logical_successstatus_vesselactivity[is.na(dataframe1$successstatus_code)] <- !dataframe1$logical_successstatus_vesselactivity[is.na(dataframe1$successstatus_code)]
   # Indicates indeterminate school must not have positive or negative success status
-  dataframe1$threshold <- "0"
   logical_successstatus_schooltype_indeterminate <- codama::vector_comparison(
     first_vector = dataframe1$schooltype_code,
-    second_vector = dataframe1$threshold,
+    second_vector = school_type,
     comparison_type = "difference",
     output = "report"
   )
   dataframe1$logical_successstatus_schooltype_indeterminate <- !logical_successstatus_schooltype_indeterminate$logical
   # Case of success status indeterminate or NA: no constraints
-  dataframe1$logical_successstatus_schooltype_indeterminate[is.na(dataframe1$successstatus_code) | dataframe1$successstatus_code == "2"] <- TRUE
+  dataframe1$logical_successstatus_schooltype_indeterminate[!(dataframe1$successstatus_code %in% success_status_school_type)] <- TRUE
   # Indicates whether the success status requires a school type
   dataframe1$logical_successstatus_schooltype <- !is.na(dataframe1$schooltype_code)
   # Case of success status NA: no constraints
@@ -1526,7 +1576,7 @@ check_operation_inspector <- function(dataframe1,
   )
   dataframe1$logical_successstatus_weight <- comparison_successstatus_weight$logical
   # Case of success status null : must not have weight (inverse of the result obtained)
-  dataframe1$logical_successstatus_weight[!is.na(dataframe1$successstatus_code) & dataframe1$successstatus_code == "0"] <- !dataframe1$logical_successstatus_weight[!is.na(dataframe1$successstatus_code) & dataframe1$successstatus_code == "0"]
+  dataframe1$logical_successstatus_weight[dataframe1$successstatus_code %in% success_status_weight] <- !dataframe1$logical_successstatus_weight[dataframe1$successstatus_code %in% success_status_weight]
   # NA success status: no constraints
   dataframe1$logical_successstatus_weight[is.na(dataframe1$successstatus_code)] <- TRUE
   # Combines test results
