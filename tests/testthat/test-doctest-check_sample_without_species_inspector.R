d02963876a1d9f6d7f7159f6552f7c8c6a742b5e
Oceane BOUHINEAU <oceane.bouhineau@ird.fr>
2025-07-18 09:27:02 +0200

    Sets minimum version package dependencies
    Sets the minimum version of package dependencies

--- tests/testthat/test-doctest-check_sample_without_species_inspector.R
+++ tests/testthat/test-doctest-check_sample_without_species_inspector.R
@@ -7,7 +7,7 @@ test_that("Doctest: check_sample_without_species_inspector", {
   # Source line: 3931
   dataframe1 <- data.frame(sample_id = c("1", "2"))
   dataframe2 <- data.frame(samplespecies_id = c("1", "2"), sample_id = c("1", "1"))
-  expect_equal(check_sample_without_species_inspector(dataframe1, dataframe2, output = "report"), structure(list(sample_id = c("1", "2"), logical = c(TRUE, FALSE)), row.names = c(
-    NA, -2L), class = "data.frame"))
+  expect_equal(check_sample_without_species_inspector(dataframe1, dataframe2, output = "report"), structure(list(sample_id = c("1", "2"), logical = c(TRUE,
+    FALSE)), row.names = c(NA, -2L), class = "data.frame"))
 })
 
